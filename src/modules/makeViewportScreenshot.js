edb8090c595c8b3f309a580839e99c5f5805a329
Tom Coleman <tom@percolatestudio.com>
2019-03-08 17:08:34 +1100

    Add new options -- noBefore/AfterScreenshot and noPause

--- src/modules/makeViewportScreenshot.js
+++ src/modules/makeViewportScreenshot.js
@@ -11,7 +11,6 @@ import ScreenDimension from '../utils/ScreenDimension';
 
 const log = debug('wdio-screenshot:makeViewportScreenshot');
 
-
 // Note: function name must be async to signalize WebdriverIO that this function returns a promise
 export default async function makeViewportScreenshot(browser, options = {}) {
   log('start viewport screenshot');
@@ -19,18 +18,28 @@ export default async function makeViewportScreenshot(browser, options = {}) {
   // get current scroll position
   const [startX, startY] = (await browser.execute(getScrollPosition)).value;
 
-  // hide scrollbars, scroll to start, hide & remove elements, wait for render
-  await beforeScreenshot(browser, options);
+  if (!options.noBeforeScreenshot) {
+    // hide scrollbars, scroll to start, hide & remove elements, wait for render
+    await beforeScreenshot(browser, options);
+  }
 
   // get screen dimisions to determine viewport height & width
   const screenDimensions = (await browser.execute(getScreenDimensions)).value;
   const screenDimension = new ScreenDimension(screenDimensions, browser);
 
   // make screenshot of area
-  const base64Image = await makeAreaScreenshot(browser, startX, startY, screenDimension.getViewportWidth(), screenDimension.getViewportHeight());
+  const base64Image = await makeAreaScreenshot(
+    browser,
+    startX,
+    startY,
+    screenDimension.getViewportWidth(),
+    screenDimension.getViewportHeight()
+  );
 
-  // show scrollbars, show & add elements
-  await afterScreenshot(browser, options);
+  if (!options.noAfterScreenshot) {
+    // show scrollbars, show & add elements
+    await afterScreenshot(browser, options);
+  }
 
   // scroll back to original position
   await browser.execute(scroll, startX, startY);
