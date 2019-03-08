99f6f96de1ef12a0a7df58e902ebfd857f4b5efe
Tom Coleman <tom@thesnail.org>
2019-03-08 14:16:54 +1100

    Hide the scrollbars rather than using overflow hidden on the html element

--- src/scripts/scrollbars.js
+++ src/scripts/scrollbars.js
@@ -1,7 +1,20 @@
 export default function scrollbars(enabled) {
+  if (!document.__scrollbarsStyleTag) {
+    document.__scrollbarsStyleTag = document.createElement('style');
+    document.__scrollbarsStyleTag.innerHtml = 'html::-webkit-scrollbar { display: none }';
+  }
+
   if (enabled) {
-    document.documentElement.style.overflow = '';
+    document.documentElement.style['scrollbar-width'] = null;
+    document.documentElement.style['-ms-overflow-style'] = null;
+    document.head.appendChild(document.__scrollbarsStyleTag);
   } else {
-    document.documentElement.style.overflow = 'hidden';
+    document.documentElement.style['scrollbar-width'] = 'none';
+    document.documentElement.style['-ms-overflow-style'] = 'none';
+    try {
+      document.head.removeChild(document.__scrollbarsStyleTag);
+    } catch (e) {
+      // Already removed or never inserted, nothing to worry about
+    }
   }
 }
